1cd2bc79f7cdf39d950348c3cb2e6ad95683b62f
Tom Preston <tom.preston77@gmail.com>
2023-05-02 13:07:59 -0400

    Initial structure

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WikiPageViews from './components/WikiPageViews';
+
+const App = () => {
+    return (
+      <div className='container'>
+        <WikiPageViews />
+      </div>
+    )
+};
+
+export default App;
---
+++ client/src/components/WikiPageViews.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WikiPageViews = () => {
+    return (
+        <div className='container'>
+            <h1>Grow Take Home</h1>
+        </div>
+    )
+}
+
+export default WikiPageViews;
